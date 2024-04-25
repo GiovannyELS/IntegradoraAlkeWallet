e84a4aa21632948e771763eb224e1d789ede703d
lirag <glira27@gmail.com>
2024-04-24 23:44:01 -0400

    SegundaVersion

--- app/src/main/java/com/example/alkewalletapp/MainActivity.java
+++ app/src/main/java/com/example/alkewalletapp/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.loginsignup);
+        setContentView(R.layout.login_page);
     }
 }
