3f0b6583e50b5236f2ec4a9236643a72251d6d26
lirag <glira27@gmail.com>
2024-05-01 23:12:19 -0400

    cuarta version las 9 vistas

---
+++ app/src/main/java/com/example/alkewalletapp/fragmentos/SendMoney.java
@@ -0,0 +1,34 @@
+package com.example.alkewalletapp.fragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.alkewalletapp.R;
+
+public class SendMoney extends Fragment {
+
+
+    public SendMoney() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_send_money, container, false);
+    }
+}
